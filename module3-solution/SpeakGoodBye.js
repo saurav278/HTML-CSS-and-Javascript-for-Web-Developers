492fa360719c5ff864dde2ac721da9d8c778548d
saurav278 <52128941+saurav278@users.noreply.github.com>
2020-06-08 01:15:38 +0530

    Delete SpeakGoodBye.js

--- module3-solution/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window) {
-	var speakWord = "Good Bye";
-	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	
-	window.byeSpeaker = byeSpeaker;
-
-})(window);
